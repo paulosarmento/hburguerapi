b3253970e9b97d271bd4cd5713436982c8c29904
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2022-03-03 21:09:52 -0300

    Migrations

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { users_user_type } from '@prisma/client';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @ApiProperty()
+  street?: string;
+
+  @ApiProperty()
+  number?: string;
+
+  @ApiProperty()
+  complement?: string;
+
+  @ApiProperty()
+  district?: string;
+
+  @ApiProperty()
+  city?: string;
+
+  @ApiProperty()
+  uf?: string;
+
+  @ApiProperty()
+  zipcode?: string;
+
+  @ApiProperty()
+  userType?: users_user_type;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,79 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    createUserDto.password = bcrypt.hashSync(createUserDto.password, 10);
+
+    const userCreated = await this.prisma.users.create({
+      data: { ...createUserDto, user_type: createUserDto.userType },
+    });
+
+    delete userCreated.password;
+
+    return userCreated;
+  }
+
+  findAll() {
+    return this.prisma.users.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        street: true,
+        number: true,
+        complement: true,
+        district: true,
+        city: true,
+        uf: true,
+        zipcode: true,
+        user_type: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+  }
+
+  async findOne(id: number) {
+    const user = await this.prisma.users.findUnique({ where: { id } });
+
+    delete user.password;
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.prisma.users.findFirst({ where: { email } });
+
+    if (user === null) {
+      throw new BadRequestException('User not found');
+    }
+
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      updateUserDto.password = bcrypt.hashSync(updateUserDto.password, 10);
+    }
+
+    const user = await this.prisma.users.update({
+      where: { id },
+      data: { ...updateUserDto },
+    });
+
+    delete user.password;
+
+    return user;
+  }
+
+  async remove(id: number) {
+    return this.prisma.users.delete({ where: { id } });
+  }
+}
